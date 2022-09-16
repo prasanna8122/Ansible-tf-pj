29a753ca263c5b3bc48e6a0d24f4c432ce143f84
prasanna8122 <113658413+prasanna8122@users.noreply.github.com>
2022-09-16 16:44:38 +0530

    Create server.tf

---
+++ server.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+region = "us-east-2"
+}
+resource "aws_instance" "myawsserver" {
+  ami = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.micro"
+  key_name = "gagan-import"
+
+  tags = {
+    Name = "Gagan-DevOps-server-IBM"
+    env = "production"
+    owner = "Gagandeep"
+  }
+  provisioner "local-exec" {
+    command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
+  }
+}
